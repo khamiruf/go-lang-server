4e145feca5e6e47bd2027a10a24c0e2f194791cd
khamiruf <khairul.amiruf@gmail.com>
2022-01-01 11:25:29 +0800

    feat: update router

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module go-lang-server
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/lib/pq v1.10.4 // indirect
-	github.com/spf13/viper v1.10.1 // indirect
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.4
+	github.com/spf13/viper v1.10.1
 )
--- router/router.go
+++ router/router.go
@@ -1,26 +1,45 @@
 package router
 
 import (
-	"fmt"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"go-lang-server/config"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const portNumber = ":8080"
 
 type Router struct {
 	config config.RouterConfig
+	logger *log.Logger
 }
 
 func NewRouter() *Router {
 	return &Router{
 		config: config.RouterConfig{},
+		logger: log.New(os.Stdout, "", 1),
 	}
 }
 
 func (router *Router) Start() {
 	r := mux.NewRouter()
-	fmt.Println("listening on port %d", portNumber)
-	http.ListenAndServe(portNumber, r)
+
+	srv := &http.Server{
+		Handler: handlers.CORS(
+			handlers.AllowedOrigins([]string{"*"}),
+			handlers.AllowedHeaders([]string{"Content-Type", "X-Auth", "x-deviceid", "x-google-token-id", "x-isd-code", "x-phone-number"}),
+			handlers.AllowedMethods([]string{http.MethodGet,
+				http.MethodDelete,
+				http.MethodPost,
+				http.MethodPut,
+				http.MethodOptions}))(r),
+		Addr:         router.config.Address,
+		WriteTimeout: router.config.WriteTimeout * time.Second,
+		ReadTimeout:  router.config.ReadTimeout * time.Second,
+	}
+	router.logger.Println("Server Started")
+	log.Fatal(srv.ListenAndServe())
 }
